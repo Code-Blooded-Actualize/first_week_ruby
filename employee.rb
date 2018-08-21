ce3b7ea57f65f592ed330ef36fdce8fa05c94ba4
JackMarx <seriousfools@gmail.com>
2018-08-21 18:32:29 -0500

    attr readers and writters

--- employee.rb
+++ employee.rb
@@ -16,6 +16,9 @@
 # =====================================
 
 class Employee
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :first_name, :last_name, :active
+
   def initialize(input_first_name, input_last_name, input_salary, input_active)
     @first_name = input_first_name
     @last_name = input_last_name
@@ -30,41 +33,13 @@ class Employee
   def give_annual_raise
     @salary = @salary * 1.05
   end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def salary
-    @salary
-  end
-
-  def active
-    @active
-  end
-
-  def active=(new_value)
-    @active = new_value
-  end
-
-  def first_name=(new_value)
-    @first_name = new_value
-  end
-
-  def last_name=(new_value)
-    @last_name = new_value
-  end
 end
 
 employee_1 = Employee.new("Bill", "McNeal", 70000, true)
 employee_2 = Employee.new("Harriet", "Hayes", 80000, true)
-p employee_1.salary
-employee_1.salary = 1000000
-p employee_1.salary
+p employee_1.first_name
+employee_1.first_name = "William"
+employee_1.print_info
 
 
 
